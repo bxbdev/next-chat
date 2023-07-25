4795bfd7f2e32efac6304d705fbc58a101d11c1e
Fiona Ye <bxbdev2022@gmail.com>
2023-07-26 02:30:49 +0800

    Added cross-env

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors:
     {
-        origin: 'http://localhost:3000',
+        origin: process.env.NODE_ENV === 'prod' ? process.env.CLIENT_URL : 'http://localhost:3000',
         methods: ["GET", "POST"]
     }
 })
